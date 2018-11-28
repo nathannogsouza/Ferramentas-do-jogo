f8521a82ec28c2cf9ec521bddead1c0ea8cfe8ec
Nathan Nogueira <42241030+nathannogsouza@users.noreply.github.com>
2018-11-28 20:11:04 -0200

    pseudomovimento

---
+++ moverplayer1.cpp
@@ -0,0 +1,71 @@
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+	return HelloWorld::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::init())
+	{
+		return false;
+	}
+	auto director = Director::getInstance();
+	auto tamtela = director->getVisibleSize();
+
+	SpriteFrameCache* framecache = SpriteFrameCache::sharedSpriteFrameCache();
+	framecache->addSpriteFramesWithFile("andando.plist");
+
+	SpriteBatchNode* spritsheet = SpriteBatchNode::create("andando.png");
+	this->addChild(spritsheet);
+
+	Vector<SpriteFrame*> animationFrames;
+
+
+	for (int i = 1; i <= 6; i++) {
+		String* filename = String::createWithFormat("andando%d.png", i);
+		SpriteFrame* frame = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName(filename->getCString());
+		animationFrames.pushBack(frame);
+	}
+
+	Animation* runAnim = Animation::createWithSpriteFrames(animationFrames, 1);
+	Sprite* andar = Sprite::createWithSpriteFrameName("andando1.png");
+
+	Size winsize = Director::sharedDirector()->getWinSize();
+	andar->setPosition(Vec2(winsize.width / 2, winsize.height / 2));
+
+	Action* action = RepeatForever::create(Animate::create(runAnim));
+
+	andar->runAction(action);
+	spritsheet->addChild(andar);
+
+
+	return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+	//Close the cocos2d-x game scene and quit the application
+	Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	exit(0);
+#endif
+
+
+}
---
+++ moverplayer2.cpp
@@ -0,0 +1,81 @@
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+	return HelloWorld::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::init())
+	{
+		return false;
+	}
+	getAnimation(std::string path, std::string filename, int start, int end, float time) {
+
+		std::string pListFile = path + filename + ".plist";
+
+		std::string pngFile = path + filename + ".png";
+
+
+		auto frameCache = SpriteFrameCache::sharedSpriteFrameCache();
+
+		frameCache->addSpriteFramesWithFile(pListFile);
+
+
+		Vector<SpriteFrame*> animationFrames;
+
+		std::stringstream ss;
+
+
+		for (int i = start; i <= end; i++) {
+
+			ss.str("");
+
+			ss << filename << i << ".png";
+
+
+			auto frame = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName(ss.str());
+
+			animationFrames.pushBack(frame);
+
+		}
+
+
+
+		Animation* animationSeq = Animation::createWithSpriteFrames(animationFrames, time);
+
+		return Animate::create(animationSeq);
+
+	}
+
+
+
+	return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+	//Close the cocos2d-x game scene and quit the application
+	Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	exit(0);
+#endif
+
+
+}
